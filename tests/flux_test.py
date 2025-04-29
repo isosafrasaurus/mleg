09d8c59b16c177b59e525ab1aab246d57a475833
Pierce Zhang <pzz1@loginx1.nots.rice.edu>
2025-04-29 14:59:31 -0500

    parallelize gpu using accelerate

--- tests/flux_test.py
+++ tests/flux_test.py
@@ -1,7 +1,17 @@
 from diffusers import DiffusionPipeline
+import torch
 
-pipe = DiffusionPipeline.from_pretrained("black-forest-labs/FLUX.1-dev")
+pipe = DiffusionPipeline.from_pretrained(
+    "black-forest-labs/FLUX.1-dev",
+    use_auth_token=True,
+    torch_dtype=torch.float16,
+    low_cpu_mem_usage=True,
+)
+
+pipe.parallelize()
+pipe.enable_attention_slicing()
 
 prompt = "Astronaut in a jungle, cold color palette, muted colors, detailed, 8k"
-image = pipe(prompt).images[0]
-print(image)
+img = pipe(prompt).images[0]
+img.save("out.png")
+print("Done! Saved to out.png")
