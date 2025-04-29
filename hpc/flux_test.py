cabf9e41b210db357a85255e63946fc0fcca6bd0
Pierce Zhang <pierce.z.zhang@gmail.com>
2025-04-29 16:04:22 -0500

    nonfunctional - cuda runs out of memory

--- hpc/flux_test.py
+++ hpc/flux_test.py
@@ -3,9 +3,14 @@ import torch, os
 from huggingface_hub import snapshot_download
 
 repo = snapshot_download("black-forest-labs/FLUX.1-dev", token=os.environ["HF_TOKEN"])
-pipe = DiffusionPipeline.from_pretrained(repo, low_cpu_mem_usage=True)
+pipe = DiffusionPipeline.from_pretrained(
+    repo,
+    torch_dtype=torch.float16,
+    low_cpu_mem_usage=True
+)
 
 pipe.enable_attention_slicing()
+pipe.to("cuda")
 
 prompt = "Astronaut in a jungle, cold color palette, muted colors, detailed, 8k"
 img = pipe(prompt).images[0]
